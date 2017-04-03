073e67c7f605513916b1010588dc41858eba2ca3
Peter Paskowsky <peter.paskowsky@gmail.com>
2017-04-03 13:44:42 +1200

    added db

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from __future__ import print_function
-import httplib2, requests, bs4, os, re, shutil, io, uuid
+import httplib2, requests, bs4, os, re, shutil, io, uuid, datetime, urlparse
 
 from apiclient.http import MediaIoBaseDownload
 from apiclient.http import MediaFileUpload
 from apiclient import discovery
+
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 from oauth2client.service_account import ServiceAccountCredentials
 
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 import web_scraper
 import httpserver
@@ -49,6 +53,53 @@ scraper_object.scrape('http://mirror.rackspace.com/archlinux/iso/latest/','torre
 scraper_object.scrape('http://www.ubuntu.com/download/alternative-downloads',"download-torrent",'false',DOWNLOAD_FOLDER)
 # Download Kali
 
+
+#########################################
+# Write Dyno Startup Time to Postgres   #
+#########################################
+
+#pull postgres url from heroku environment variable
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+db_url = (url.netloc)
+database = (url.path)
+final_url = ('postgresql://'+db_url+database)
+print (final_url)
+
+#Create a DBAPI connection
+engine = create_engine(final_url)
+
+#Declare an instance of the Base class for mapping tables
+Base = declarative_base()
+
+#Map a table to a class by inheriting base class
+class Logs(Base):
+    __tablename__ = "Logs"
+
+    Id = Column(Integer, primary_key=True)
+    AccessTime = Column(DateTime)
+
+#create all tables if they do not exist
+Base.metadata.create_all(engine)
+
+#create session to DB        
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#execute commands via sqlalchemy orm
+command1 = Logs(AccessTime=datetime.datetime.now())
+session.add(command1)
+session.commit()
+
+#query via sqlalchemy orm
+response = session.query(Logs).all()
+
+for x in response:
+    print x.AccessTime
+
+#Close the connection
+engine.dispose()
 
 ######################################
 # Start Web Server                   #
